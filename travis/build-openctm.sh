9e0d0a8a47e4cf41357590432e75663d69f8221e
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2018-08-12 20:56:20 +0200

    updated linux build

--- travis/build-openctm.sh
+++ travis/build-openctm.sh
@@ -1,4 +1,10 @@
-yum install libgtk2.0-dev
+# ./build-openctm.sh $MAKEFILE $LIB_NAME
+
+# If on Linux, install GTK2
+if [[ uname == 'Linux' ]]; then
+  yum install gtk2-devel -y
+fi
+
 git clone https://github.com/Danny02/OpenCTM.git openctm_tmp
 make -C openctm_tmp -f $1
 mkdir openctm/libs
