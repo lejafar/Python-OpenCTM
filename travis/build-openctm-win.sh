4cc02b651293f355c99b3203694cfd87ef9d22e6
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2018-08-19 14:01:18 +0200

    another attempt at compiling on windows

--- travis/build-openctm-win.sh
+++ travis/build-openctm-win.sh
@@ -1,6 +1,6 @@
 # ./build-openctm-win.sh
 
 git clone https://github.com/Danny02/OpenCTM.git openctm_tmp
-mingw32-make -f Makefile.mingw
+nmake /f Makefile.msvc
 mkdir openctm/libs
 mv openctm_tmp/lib/libopenctm.dll openctm/libs
