4e19f76a7b697654cdab973d2aae50e48eebe82b
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2019-09-27 09:11:52 +0200

    update versioning once again

--- openctm/version.py
+++ openctm/version.py
@@ -2,4 +2,4 @@ from datetime import datetime, date
 
 today = date.today()
 now = datetime.now()
-__version__ = f'1.0.10a{today.year}{today.month}{today.day}'
+__version__ = f'1.1.0a{today.year}{today.month}{today.day}'
