e94678eef4494f28d22c73f1eedfff58e2e89c4a
Rafael Hautekiet <rafaelhautekiet@gmail.com>
2020-06-15 13:55:57 +0200

    fix tests and bump version

--- openctmpy/version.py
+++ openctmpy/version.py
@@ -2,4 +2,5 @@ from datetime import datetime, date
 
 today = date.today()
 now = datetime.now()
-__version__ = f'1.1.0a{today.year}{today.month}{today.day}'
+#__version__ = f'1.1.0a{today.year}{today.month}{today.day}'
+__version__ = f'1.1.1'
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,12 +2,12 @@ import pathlib
 import numpy as np
 import trimesh
 
-import openctm
+import openctmpy
 
 def data_path(): return pathlib.Path(__file__).parent / 'test-data'
 
 def test_basic_import_MG1():
-    mesh = openctm.CTM.load(data_path() / 'squares.ctm')
+    mesh = openctmpy.CTM.load(data_path() / 'squares.ctm')
 
     assert mesh.header.compression_method == b'MG1'
     assert mesh.vertices.shape == (124, 3)
@@ -15,14 +15,14 @@ def test_basic_import_MG1():
 
 def test_import_MG1_export_RAW_import_RAW():
     # import MG1
-    mesh_MG1 = openctm.CTM.load(data_path() / 'squares.ctm')
+    mesh_MG1 = openctmpy.CTM.load(data_path() / 'squares.ctm')
 
     # export RAW
     mesh_MG1.header.compression_method = "RAW"
     mesh_MG1.export(data_path() / 'squares_RAW.ctm')
 
     # import RAW
-    mesh_RAW = openctm.CTM.load(data_path() / 'squares_RAW.ctm')
+    mesh_RAW = openctmpy.CTM.load(data_path() / 'squares_RAW.ctm')
 
     assert mesh_MG1.vertices.shape == mesh_RAW.vertices.shape
     assert mesh_MG1.faces.shape == mesh_RAW.faces.shape
@@ -34,9 +34,9 @@ def test_round_trip():
     mesh = trimesh.load(str(data_path() / 'box.stl'))
 
     # export-import
-    ctm = openctm.CTM(mesh.vertices, mesh.faces)
+    ctm = openctmpy.CTM(mesh.vertices, mesh.faces)
     ctm.export(data_path() / 'box.ctm')
-    ctm = openctm.CTM.load(data_path() / 'box.ctm')
+    ctm = openctmpy.CTM.load(data_path() / 'box.ctm')
 
     assert mesh.vertices.shape == ctm.vertices.shape
     assert mesh.faces.shape == ctm.faces.shape
@@ -44,7 +44,7 @@ def test_round_trip():
 
 def test_delta_encode_decode():
     data = np.array([[0, 1, 2], [2, 3, 4], [4, 5, 6], [6, 7, 8]]).reshape(-1)
-    encoded_data = openctm.utils.delta_encode(data)
-    decoded_data = openctm.utils.delta_decode(encoded_data)
+    encoded_data = openctmpy.utils.delta_encode(data)
+    decoded_data = openctmpy.utils.delta_decode(encoded_data)
     assert np.allclose(data, decoded_data)
 
